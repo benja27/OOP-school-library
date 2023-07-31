5b7a1f1af4661a6a76657ec27417f134ceddfb01
benja27 <50673336+benja27@users.noreply.github.com>
2023-07-31 15:24:34 -0600

    Create student, person and teacher classess

---
+++ person.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# class person
+class Person
+  # we set the getter and setter for these elements - id, name and age
+  attr_accessor :id, :name, :age
+
+  # constructor
+  def initialize(id, age, name = 'unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  # public methods
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+
+  # private methods
+  private
+
+
+
+  def of_age?
+    @age >= 18
+  end
+end
---
+++ teacher.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require './person'
+
+# we create the class teacher
+class Teacher < Person
+  def initialize(id, age, specialization, name = 'unknown', parent_permission: true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
