6d3b5e0fd0e0284c0c714b368b6bae62812b8dcb
benja27 <50673336+benja27@users.noreply.github.com>
2023-07-31 15:35:38 -0600

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # class person
 class Person
   # we set the getter and setter for these elements - id, name and age
--- studen.rb
+++ studen.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './person'
 # we create the class student
 class Student < Person
