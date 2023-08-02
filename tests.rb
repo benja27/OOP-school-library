07f2b2261ead661abbccfc10dfaafa4e70f3635a
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-02 17:42:04 -0600

    Update id

--- tests.rb
+++ tests.rb
@@ -1,7 +1,7 @@
 require './person'
 require './decorator'
 
-person = Person.new(22, 23, 'maximilianus')
+person = Person.new(22, 'maximilianus')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
