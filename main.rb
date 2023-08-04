49d6c54bef9b0286da2aa7719361f53f829acfff
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-03 19:07:22 -0600

    Fix linter errors

--- main.rb
+++ main.rb
@@ -26,7 +26,7 @@ class Options
   def select_option(user_choice)
     options = {
       '1' => :list_all_books, '2' => :list_all_people, '3' => :create_person,
-      '4' => :create_book, '5' => :create_rental,      '6' => :list_all_rentals, '7' => :exit
+      '4' => :create_book, '5' => :create_rental, '6' => :list_all_rentals, '7' => :exit
     }
     method = options[user_choice]
     if method.nil?
